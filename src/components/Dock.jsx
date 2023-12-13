4de72c8fd7025983c07968da22c0e57b3e0e1c5b
Maung <maung.alive@gmail.com>
2023-12-13 22:58:34 +0630

    Dock spreator

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -4,6 +4,7 @@ export default function Dock() {
   return (
     <div className="fixed left-1/2 bottom-0 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[60px] p-1 rounded-2xl flex justify-evenly bg-[rgba(255,255,255,0.4)] border border-white">
         <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="border-r"></div>
         <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
         <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
         <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
