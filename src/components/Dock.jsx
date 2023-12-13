a3638cf25b98cbb88aa6dbf67aae33378510f243
Maung <maung.alive@gmail.com>
2023-12-13 22:57:07 +0630

    Dock style update

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Dock() {
   return (
-    <div className="fixed left-1/2 bottom-0 -translate-x-1/2 -translate-y-1/2 w-[400px] h-[60px] p-1 rounded-2xl flex justify-evenly bg-[rgba(225,225,225,0.4)]">
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
-        <div className="py-3 px-4 rounded-2xl bg-gray-500 text-white duration-150 hover:bg-gray-600 hover:rounded-lg">H</div>
+    <div className="fixed left-1/2 bottom-0 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[60px] p-1 rounded-2xl flex justify-evenly bg-[rgba(255,255,255,0.4)] border border-white">
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
+        <div className="py-3 px-4 text-gray-900 duration-150 hover:bg-gray-500 hover:rounded-2xl">H</div>
     </div>
   )
 }
