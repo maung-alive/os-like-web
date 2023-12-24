2f824ee708e69d16d8d3a90329eff4707d8a0979
Maung <maung.alive@gmail.com>
2023-12-24 11:19:33 +0630

    ContextMenu Customized

---
+++ src/components/ContextMenu.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { FaCopy, FaCut, FaInfoCircle, FaPaste, FaPowerOff, FaSort, FaSync, FaTh } from 'react-icons/fa'
+
+export default function ContextMenu({ contextMenuRef, top, left }) {
+  return (
+    <div
+        ref={contextMenuRef}
+        className="fixed z-50 py-2 px-2 bg-gray-100 rounded shadow-md w-[300px]"
+        style={{
+            top: `${top}px`,
+            left: `${left}px`,
+        }}
+    >
+        <div className='flex items-center justify-left gap-x-3 px-2 py-1'>
+            <FaCopy className='text-black hover:text-gray-500 text-lg' />
+            <FaCut className='text-black hover:text-gray-500 text-lg' />
+            <FaPaste className='text-black hover:text-gray-500 text-lg' />
+        </div>
+        <hr className='border-gray-300' />
+        <div className='mt-2'>
+            <button className="flex items-center gap-x-2 text-black text-md font-ubuntu w-full text-left hover:bg-gray-300 hover:text-gray-500 px-2 py-1 my-1 rounded">
+                <FaTh /> View
+            </button>
+            <button className="flex items-center gap-x-2 text-black text-md font-ubuntu w-full text-left hover:bg-gray-300 hover:text-gray-500 px-2 py-1 my-1 rounded">
+                <FaSort /> Sort
+            </button>
+            <button className="flex items-center gap-x-2 text-black text-md font-ubuntu w-full text-left hover:bg-gray-300 hover:text-gray-500 px-2 py-1 my-1 rounded">
+                <FaSync /> Refresh
+            </button>
+            <button className="flex items-center gap-x-2 text-black text-md font-ubuntu w-full text-left hover:bg-gray-300 hover:text-gray-500 px-2 py-1 my-1 rounded">
+                <FaInfoCircle /> About PC
+            </button>
+            <button className="flex items-center gap-x-2 text-black text-md font-ubuntu w-full text-left hover:bg-gray-300 hover:text-gray-500 px-2 py-1 my-1 rounded">
+                <FaPowerOff /> Shutdown
+            </button>
+        </div>
+    </div>
+  )
+}
--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,13 +1,47 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import { RunningApps } from '../App'
+import ContextMenu from './ContextMenu';
 
 export default function Screen({ children }) {
 
   let [ processes, setProcesses ] = useContext(RunningApps);
+  
+  let contextMenuRef = useRef(null);
+  let [ contextMenu, setContextMenu ] = useState({
+    show: false,
+    top: 0,
+    left: 0,
+  })
+
+  const contextMenuHandler = (e) => {
+    e.preventDefault();
+    setContextMenu({
+      show: true,
+      top: e.pageY,
+      left: e.pageX
+    });
+  }
+
+  const handleMouseDown = (e) => {
+    if(contextMenuRef.current.contains(e.target)){
+      return;
+    } else {
+      setContextMenu({
+        show: false
+      })
+    }
+  }
 
   return (
-    <div className='w-screen h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500' style={{ background: '/wallpaper.jpg' }}>
+    <div
+      className='w-screen h-screen bg-gradient-to-r from-violet-500 to-fuchsia-500'
+      style={{ background: '/wallpaper.jpg' }}
+      onMouseDown={(e) => handleMouseDown(e)}
+      onContextMenu={e => contextMenuHandler(e)}
+    >
         {children}
+
+        {contextMenu.show && <ContextMenu contextMenuRef={contextMenuRef} top={contextMenu.top} left={contextMenu.left} />}
 
         {processes.filter( i => i.minimized == false ).map(
           process => process.component
